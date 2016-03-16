7307f454b4ed765922a689071736637ee974f104
David Wang <DevilDavidWang@gmail.com>
2016-03-16 04:14:50 -0400

    create a special string that handle escapes

---
+++ escape_string.cc
@@ -0,0 +1,52 @@
+/**
+ *    The MIT License (MIT)
+ *
+ *    Copyright (c) 2016 Kaiqiang Wang
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in all
+ *    copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *    SOFTWARE.
+ */
+
+#include "escape_string.h"
+
+#include <string>
+#include <vector>
+
+namespace wish {
+
+using std::vector;
+using std::string;
+
+EscapeString::EscapeString(const string& str) {
+    _str.reserve(str.size());
+    _active = vector<bool>(str.size(), true);
+    size_t pos = 0, found = 0;
+
+    while((found = str.find('\\', pos)) != string::npos) {
+        _str += str.substr(pos, found - pos);
+
+        pos = found + 1;
+        if (pos >= str.size()) break;
+        _str.push_back(str[pos]);
+        _active[_str.size() - 1] = false;
+        pos = found + 2;
+    }
+    if (pos < str.size()) _str += str.substr(pos, str.size() - pos);
+}
+
+} /* wish */ 
